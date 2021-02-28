bf1cd8630eb1878f8c3ba95a0f5367d4135310b3
huanhuanai <695045212@qq.com>
2021-02-28 21:29:32 +0800

    messageLabel commit

--- Homepwner/HHAHypnosisViewController/HHACircleViewController.m
+++ Homepwner/HHAHypnosisViewController/HHACircleViewController.m
@@ -9,7 +9,7 @@
 #import "HHACircleViewController.h"
 #import "HHAHypnosisView.h"
 
-@interface HHACircleViewController ()
+@interface HHACircleViewController ()<UITextFieldDelegate>
 
 @end
 
@@ -42,8 +42,40 @@
     [self.view addSubview:scrollView];
     [scrollView addSubview:hv];
     [scrollView addSubview:shv];
+    
+    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(10, 100, frame.size.width - 20, 45)];
+    [hv addSubview:textField];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"Hypnotize me";
+    textField.returnKeyType = UIReturnKeyDone;
+    textField.delegate = self;
 }
 
 
+- (void)drawHypnoticMessage:(NSString *)message {
+    for (int i = 0; i < 20; i++) {
+        UILabel *messageLabel = [[UILabel alloc] init];
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor redColor];
+        messageLabel.text = message;
+        [messageLabel sizeToFit];
+        NSInteger width = (NSInteger)(self.view.bounds.size.width - messageLabel.bounds.size.width);
+        NSInteger x = arc4random() % width;
+        NSInteger height = (NSInteger)(self.view.bounds.size.height - messageLabel.bounds.size.width);
+        NSInteger y = arc4random() % height;
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        [self.view addSubview:messageLabel];
+    }
+}
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self drawHypnoticMessage:textField.text];
+    textField.text = @"";
+    [textField resignFirstResponder];
+    return YES;
+}
 
 @end
--- Homepwner/HHAHypnosisViewController/HHAHypnosisView.m
+++ Homepwner/HHAHypnosisViewController/HHAHypnosisView.m
@@ -27,40 +27,30 @@
     return self;
 }
 
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
     CGRect bounds = self.bounds;
 
-    // Figure out the center of the bounds rectangle
     CGPoint center;
     center.x = bounds.origin.x + bounds.size.width / 2.0;
     center.y = bounds.origin.y + bounds.size.height / 2.0;
 
-    // The largest circle will circumstribe the view
-    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
+    CGFloat maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
 
-    UIBezierPath *path = [[UIBezierPath alloc] init];
+    UIBezierPath *bp = [UIBezierPath bezierPath];
 
-    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
-        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
-
-        [path addArcWithCenter:center
+    for (CGFloat currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        [bp moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+        [bp addArcWithCenter:center
                         radius:currentRadius
                     startAngle:0.0
                       endAngle:M_PI * 2.0
                      clockwise:YES];
     }
 
-    // Configure line width to 10 points
-    path.lineWidth = 10;
-
-    // Configure the drawing color to light gray
+    bp.lineWidth = 10;
     [self.circleColor setStroke];
-
-    // Draw the line!
-    [path stroke];
+    [bp stroke];
 }
 
 - (void)setCircleColor:(UIColor *)circleColor
@@ -72,12 +62,9 @@
 // When a finger touches the screen
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    NSLog(@"Touch began on %@", self);
-
-    // Get 3 random numbers between 0 and 1
-    float red = (arc4random() % 100) / 100.0;
-    float green = (arc4random() % 100) / 100.0;
-    float blue = (arc4random() % 100) / 100.0;
+    CGFloat red = (arc4random() % 100) / 100.0;
+    CGFloat green = (arc4random() % 100) / 100.0;
+    CGFloat blue = (arc4random() % 100) / 100.0;
 
     UIColor *randomColor = [UIColor colorWithRed:red
                                            green:green
